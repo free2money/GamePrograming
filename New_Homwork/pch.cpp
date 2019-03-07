39a57987943c9eec431d9d590cc4f7cd887c3182
free2money <38486282+free2money@users.noreply.github.com>
2019-03-07 12:27:01 +0900

    190307

---
+++ New_Homwork/pch.cpp
@@ -0,0 +1,35 @@
+﻿// pch.cpp: 미리 컴파일된 헤더에 해당하는 소스 파일입니다. 성공하려면 컴파일이 필요합니다.
+
+#include "pch.h"
+
+// 일반적으로 이 파일을 무시하지만 미리 컴파일된 헤더를 사용하는 경우 유지합니다.
+
+void quicksort(int *a, int start, int length) {
+	int i = start;
+	int temp = 0, v = length;
+	int j = length;
+
+	if (start >= length) return;
+
+	while (i <= j) {
+		while (a[i] < a[v]) {
+			i++;
+		}
+		while (a[j] > a[v]) {
+			j--;
+		}
+		if (i >= j) break;
+			temp = a[i];
+			a[i] = a[j];
+			a[j] = temp;
+	}
+
+	if (a[i] > a[v]) {
+		temp = a[i];
+		a[i] = a[v];
+		a[v] = temp;
+	}
+
+	quicksort(a, start, i-1);
+	quicksort(a, i+1, length);
+}
